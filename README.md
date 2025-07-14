13ce5b31035667834665b7038169a53195311fdb
Michalis Kamburelis <michalis.kambi@gmail.com>
2025-07-14 11:30:43 +0200

    Demo with 1000 cubes with varying size and positions

---
+++ README.md
@@ -0,0 +1,31 @@
+# My New Project
+
+Simplest cube creation / modification / removal for https://forum.castle-engine.io/t/drawing-many-cubes-faster/ .
+
+Please note that this doesn't try to be fast / optimal. And all cubes here have different size and color. E.g.
+
+- This example doesn't try to group cubes and render them using `TCastleTransformReference`. In many actual cases, you don't have 1000 different objects, you have 1000 instances of the few same object(s), and you can optimize it with `TCastleTransformReference`.
+
+- It doesn't use LOD.
+
+- It doesn't use batching.
+
+- It doesn't instantiate a design using `TCastleComponentFactory` mentioned in that thread. In many practical cases, `TCastleBox` will not be enough, instead: instantiate designs with https://castle-engine.io/reuse_design and/or create `TCastleScene` with generated node graph inside.
+
+- Here, each cube is separate `TCastleBox` instance (which is internally separate `TCastleScene` instance). This is sometimes enough, but sometimes you can put multiple cubes in one scene.
+
+It's just a simplest example how to easily create / modify / remove many cubes.
+
+Using [Castle Game Engine](https://castle-engine.io/).
+
+## Building
+
+Compile by:
+
+- [CGE editor](https://castle-engine.io/editor). Just use menu items _"Compile"_ or _"Compile And Run"_.
+
+- Or use [CGE command-line build tool](https://castle-engine.io/build_tool). Run `castle-engine compile` in this directory.
+
+- Or use [Lazarus](https://www.lazarus-ide.org/). Open in Lazarus `many_cubes_standalone.lpi` file and compile / run from Lazarus. Make sure to first register [CGE Lazarus packages](https://castle-engine.io/lazarus).
+
+- Or use [Delphi](https://www.embarcadero.com/products/Delphi). Open in Delphi `many_cubes_standalone.dproj` file and compile / run from Delphi. See [CGE and Delphi](https://castle-engine.io/delphi) documentation for details.
---
+++ code/gameviewmain.pas
@@ -0,0 +1,120 @@
+{ Main view, where most of the application logic takes place.
+
+  Feel free to use this code as a starting point for your own projects.
+  This template code is in public domain, unlike most other CGE code which
+  is covered by BSD or LGPL (see https://castle-engine.io/license). }
+unit GameViewMain;
+
+interface
+
+uses Classes,
+  CastleVectors, CastleComponentSerialize,
+  CastleUIControls, CastleControls, CastleKeysMouse, CastleViewport,
+  CastleScene, CastleTransform, CastleUtils, CastleLog;
+
+type
+  { Main view, where most of the application logic takes place. }
+  TViewMain = class(TCastleView)
+  published
+    { Components designed using CGE editor.
+      These fields will be automatically initialized at Start. }
+    LabelFps: TCastleLabel;
+    Viewport1: TCastleViewport;
+    BoxesParent: TCastleTransform;
+  public
+    constructor Create(AOwner: TComponent); override;
+    procedure Start; override;
+    procedure Update(const SecondsPassed: Single; var HandleInput: Boolean); override;
+    function Press(const Event: TInputPressRelease): Boolean; override;
+  end;
+
+var
+  ViewMain: TViewMain;
+
+implementation
+
+uses SysUtils;
+
+{ TViewMain ----------------------------------------------------------------- }
+
+constructor TViewMain.Create(AOwner: TComponent);
+begin
+  inherited;
+  DesignUrl := 'castle-data:/gameviewmain.castle-user-interface';
+end;
+
+procedure TViewMain.Start;
+
+  function CreateBox: TCastleBox;
+  var
+    Size: Single;
+  begin
+    Result := TCastleBox.Create(FreeAtStop);
+    Result.Translation := Vector3(
+      RandomFloatRange(-10, 10),
+      RandomFloatRange(-10, 10),
+      RandomFloatRange(-10, 10)
+    );
+
+    // randomize size
+    Size := RandomFloatRange(1, 3);
+    Result.Size := Vector3(Size, Size, Size);
+
+    // randomize color
+    Result.Color := Vector4(
+      RandomFloatRange(0, 1),
+      RandomFloatRange(0, 1),
+      RandomFloatRange(0, 1),
+      1.0
+    );
+  end;
+
+const
+  NumCubes = 1000;
+var
+  I: Integer;
+begin
+  inherited;
+  for I := 0 to NumCubes - 1 do
+    BoxesParent.Add(CreateBox);
+end;
+
+procedure TViewMain.Update(const SecondsPassed: Single; var HandleInput: Boolean);
+begin
+  inherited;
+  { This virtual method is executed every frame (many times per second). }
+  Assert(LabelFps <> nil, 'If you remove LabelFps from the design, remember to remove also the assignment "LabelFps.Caption := ..." from code');
+  LabelFps.Caption := 'FPS: ' + Container.Fps.ToString;
+end;
+
+function TViewMain.Press(const Event: TInputPressRelease): Boolean;
+var
+  RandomCubeIndex: Integer;
+begin
+  Result := inherited;
+  if Result then Exit; // allow the ancestor to handle keys
+
+  // key Q removes box under mouse
+  if Event.IsKey(keyQ) then
+  begin
+    if Viewport1.TransformUnderMouse is TCastleBox then
+      Viewport1.TransformUnderMouse.Free; // this will also remove it from Viewport1.Items
+    Exit(true); // key was handled
+  end;
+
+  // key E changes random cube's color, accesses cube by index
+  if Event.IsKey(keyE) and (BoxesParent.Count <> 0) then
+  begin
+    RandomCubeIndex := Random(BoxesParent.Count);
+    (BoxesParent[RandomCubeIndex] as TCastleBox).Color := Vector4(
+      RandomFloatRange(0, 1),
+      RandomFloatRange(0, 1),
+      RandomFloatRange(0, 1),
+      1.0
+    );
+    WritelnLog('Changed color of cube %d', [RandomCubeIndex]);
+    Exit(true); // key was handled
+  end;
+end;
+
+end.
